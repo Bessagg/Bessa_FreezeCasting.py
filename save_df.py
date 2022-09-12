7d51be9749eee25b7c39556e35795a649f831d9f
Bessagg <bessaraid@gmail.com>
2022-09-11 23:58:36 -0300

    New df with calculated volume fraction according to LiuYue

--- save_df.py
+++ save_df.py
@@ -2,6 +2,11 @@ import pickle
 from database2dataframe import db_to_df
 
 df = db_to_df().copy()
-pkl_filename = "freeze_casting_df.pkl"
+df['vf_solid'] = df['vf_part_1']/(df['vf_part_1'] + df['vf_fluid_1'])
+df.drop('vf_part_1', axis=1, inplace=True)
+df.drop('vf_fluid_1', axis=1, inplace=True)
+pkl_filename = "freeze_casting_df_v2.0.pkl"
 with open(pkl_filename, 'wb') as file:
     pickle.dump(df, file)
+
+print(f"saved df as {pkl_filename}")
